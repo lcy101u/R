9da76734ab46f0169c511d7728917daed1cb3869
Rain <phanton0117@gmail.com>
2018-12-17 11:19:41 +0800

    Create dataframe.R

---
+++ dataframe.R
@@ -0,0 +1,9 @@
+#data.frame
+
+id <- c(1, 2, 3, 4)
+name <- c("趙一傷", "錢二敗", "孫三毀", "李四衰")
+age <- c(25, 31, 22, 28)
+gender <- c("M", "M", "F", "M")
+paycheck <- c(25000, 40000, 22000, 37000)
+employee.DF <- data.frame(id, name, age, gender, paycheck)
+employee.DF
